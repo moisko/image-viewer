093ad6604ce1ed6884c73cf03b06af6b118766e0
moisko <boris.r.borisov@gmail.com>
2018-07-22 07:50:28 +0300

    remove default constructor

--- src/main/java/bg/borisborisov/imageviewer/controller/exception/ImageNotFoundException.java
+++ src/main/java/bg/borisborisov/imageviewer/controller/exception/ImageNotFoundException.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ImageNotFoundException extends Exception {
-
-    public ImageNotFoundException() {
-        super();
-    }
-
     public ImageNotFoundException(String message) {
         super(message);
     }
