b9c1449004c83ac1778a53ffccada91943147198
moisko <boris.r.borisov@gmail.com>
2018-07-22 07:48:23 +0300

    add lombok data annotation to Image domain entity so it can me properly serialized

--- src/main/java/bg/borisborisov/imageviewer/model/Image.java
+++ src/main/java/bg/borisborisov/imageviewer/model/Image.java
@@ -1,11 +1,13 @@
 package bg.borisborisov.imageviewer.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document
 @NoArgsConstructor
 @AllArgsConstructor
