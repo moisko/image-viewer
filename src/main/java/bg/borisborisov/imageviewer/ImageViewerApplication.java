b7d4e7893de6ef8fe3c68327aeadac0fc230bd5a
moisko <boris.r.borisov@gmail.com>
2018-07-23 14:55:48 +0300

    add embedded mongo

--- src/main/java/bg/borisborisov/imageviewer/ImageViewerApplication.java
+++ src/main/java/bg/borisborisov/imageviewer/ImageViewerApplication.java
@@ -1,31 +1,10 @@
 package bg.borisborisov.imageviewer;
 
-import bg.borisborisov.imageviewer.model.Image;
-import bg.borisborisov.imageviewer.repository.ImageRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
-import java.util.UUID;
 
 @SpringBootApplication
 public class ImageViewerApplication {
-
-    @Bean
-    CommandLineRunner importData(ImageRepository imageRepository) {
-
-        return args ->
-                imageRepository.deleteAll().thenMany(
-                        imageRepository.saveAll(Arrays.asList(
-                                new Image(UUID.randomUUID().toString(), "im1"),
-                                new Image(UUID.randomUUID().toString(), "im2"),
-                                new Image(UUID.randomUUID().toString(), "im3"),
-                                new Image(UUID.randomUUID().toString(), "im4")
-                        ))
-                );
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ImageViewerApplication.class, args);
--- src/test/java/bg/borisborisov/imageviewer/controller/ImageControllerTest.java
+++ src/test/java/bg/borisborisov/imageviewer/controller/ImageControllerTest.java
@@ -3,8 +3,10 @@ package bg.borisborisov.imageviewer.controller;
 import bg.borisborisov.imageviewer.model.Image;
 import bg.borisborisov.imageviewer.model.ImageEvent;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -12,24 +14,34 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ImageControllerTest {
-
-    @Autowired
-    private ImageController imageController;
 
     @Autowired
     private MongoOperations mongoOperations;
 
-    // Client consuming the Reactive REST Endpoint
+    @Autowired
+    private ImageController imageController;
+
     private WebTestClient client;
 
     @Before
     public void setup() {
         mongoOperations.dropCollection(Image.class);
+
+        Arrays.asList(
+                new Image(UUID.randomUUID().toString(), "im1"),
+                new Image(UUID.randomUUID().toString(), "im2"),
+                new Image(UUID.randomUUID().toString(), "im3"),
+                new Image(UUID.randomUUID().toString(), "im4")
+        ).forEach(mongoOperations::insert);
 
         client = WebTestClient.bindToController(imageController).build();
     }
